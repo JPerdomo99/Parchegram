ab8ddb846fa053e6348e67f8715cdcf71904b609
atehortua199 <atehortua199@outlook.com>
2021-01-04 15:46:39 -0500

    Se modifico la cadena de conexion que se conecta al hosting

--- Parchegram.Model/Models/ParchegramDBContext.cs
+++ Parchegram.Model/Models/ParchegramDBContext.cs
@@ -32,7 +32,7 @@ namespace Parchegram.Model.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=localhost;Database=ParchegramDB;Trusted_Connection=True");
+                optionsBuilder.UseSqlServer("Server=sql5066.site4now.net;Database=DB_A6D47F_julian1999;User Id=DB_A6D47F_julian1999_admin;Password=julian1999");
             }
         }
 
